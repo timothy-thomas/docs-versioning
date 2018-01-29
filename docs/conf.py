e036ae8a109f6e2399246d6c602ea3c3c8182ae7
Tim Thomas <tthomas@metamorphsoftware.com>
2018-01-29 15:25:43 -0600

    Added some configuration options.

--- docs/conf.py
+++ docs/conf.py
@@ -3,3 +3,12 @@ master_doc = 'index'
 
 import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
+
+import re
+scv_whitelist_branches = ("master", "dev")
+scv_whitelist_tags = (re.compile(r'^v\d+\.\d+\.\d+$'),)
+
+scv_greatest_tag = True
+
+scv_show_banner = True
+scv_banner_greatest_tag = True
